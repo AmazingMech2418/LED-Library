c71b14608ed237a6e3644da05c8fa4359964275f
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-06-18 16:57:48 -0400

    Update ledultra.h

--- ledultra.h
+++ ledultra.h
@@ -3,7 +3,31 @@
 
 #include "Arduino.h"
 
-Class LEDultraclass
+Class LED
 {
-
+  public:
+    LED(int pin);
+    void on();
+    void off();
+  private:
+    int _pin;
 }
+
+Class LEDultra
+{
+  public:
+    LEDultra(int a, int b, int c, int d, int e, int f, int g, int h, int i);
+    void array(int j, int k, int l, int m, int n, int o, int p, int q, int r);
+  private:
+    int _a;
+    int _b;
+    int _c;
+    int _d;
+    int _e;
+    int _f;
+    int _g;
+    int _h;
+    int _i;
+}
+
+#endif
