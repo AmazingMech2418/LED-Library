c3a7d081f4b0ec17df3aa7e2c063327a19553bd3
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-06-18 17:05:34 -0400

    Update ledultra.cpp

--- ledultra.cpp
+++ ledultra.cpp
@@ -1,5 +1,17 @@
+#include "Arduino.h"
+#include "ledultra.h"
+
 LED::LED(int pin)
 {
   pinMode(pin, OUTPUT);
   _pin = pin;
+}
+void LED::on()
+{
+  digitalWrite(_pin, HIGH);
+}
+
+void LED::off()
+{
+  digitalWrite(_pin, LOW);
 }
